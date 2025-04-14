99de8e188676726f0121c0862189d7815818ed01
Tamas Kardos <totoka03@gmail.com>
2025-04-14 14:00:24 +0200

    initial commit

---
+++ src/main/kotlin/com/datelocator/datelocatorbe/DatelocatorBeApplication.kt
@@ -0,0 +1,11 @@
+package com.datelocator.datelocatorbe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DatelocatorBeApplication
+
+fun main(args: Array<String>) {
+	runApplication<DatelocatorBeApplication>(*args)
+}
