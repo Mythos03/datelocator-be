b91bdd5fabb65429812e1b3815050e79635b8e6a
Tamás Kardos <tamas.kardos@ingenimind.com>
2025-07-03 20:09:21 +0200

    Add logging and detailed error handling to ReviewController.

--- src/main/kotlin/com/datelocator/datelocatorbe/review/ReviewController.kt
+++ src/main/kotlin/com/datelocator/datelocatorbe/review/ReviewController.kt
@@ -3,6 +3,8 @@ package com.datelocator.datelocatorbe.review
 import com.datelocator.datelocatorbe.review.models.Review
 import com.datelocator.datelocatorbe.review.models.ReviewRequestDto
 import com.datelocator.datelocatorbe.review.models.ReviewResponseDto
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -14,13 +16,25 @@ import org.springframework.web.bind.annotation.RestController
 class ReviewController (
     private val reviewService: ReviewService
 ){
+    private val logger = LoggerFactory.getLogger(ReviewController::class.java)
+
     @PostMapping
-    fun createReview(@RequestBody reviewRequestDto: ReviewRequestDto): ResponseEntity<ReviewResponseDto?> {
+    fun createReview(@RequestBody reviewRequestDto: ReviewRequestDto): ResponseEntity<Any> {
         return try {
+            logger.info("Received review creation request")
             val review = reviewService.createReview(reviewRequestDto)
+            logger.info("Review created successfully")
             ResponseEntity.ok(review)
         } catch (e: IllegalArgumentException) {
-            ResponseEntity.badRequest().build()
+            logger.error("Failed to create review: Bad request", e)
+            ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(mapOf("error" to e.message))
+        } catch (e: Exception) {
+            logger.error("Failed to create review", e)
+            ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(mapOf("error" to e.message))
         }
     }
 }
